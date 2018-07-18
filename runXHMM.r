5cf6a79dd8b6b3df79bdf57f8d17f9ffc5e6607f
johnbwaldron <39961096+johnbwaldron@users.noreply.github.com>
2018-07-18 15:40:18 -0500

    Update runXHMM.r

--- runXHMM.r
+++ runXHMM.r
@@ -4,6 +4,7 @@
 run GATK
 
 ########Kirsten's command line entry - i don't get all the pieces.################
+# Kirsten and the XHMM tutorial used GATK 3.8 for read depth and preparation for XHMM: I am going to use 4.0
 find ./ -name "*.bam" | parallel 'java -Xmx3072m -jar GenomeAnalysisTK.jar\
 -T DepthOfCoverage -I {} -L /home/BIO/johnw/GELCC_WES_RAW_Data/Agilent_SureSelect_v5UTRs_edited.bed \
 -R ucsc.hg19.fasta -dt BY_SAMPLE -dcov 5000 -l INFO --omitDepthOutputAtEachBase --omitLocusTable \
@@ -12,7 +13,7 @@ find ./ -name "*.bam" | parallel 'java -Xmx3072m -jar GenomeAnalysisTK.jar\
 
 ##################### from XHMM documentation ########################################
 
-find /home/BIO/johnw/GELCC_WES_RAW_Data/2_GELCC_bam_files -name "*.bam" | java -Xmx3072m -jar GenomeAnalysisTK.jar \
+find /home/BIO/johnw/GELCC_WES_RAW_Data/2_GELCC_bam_files -name "*.bam" | 
 -T DepthOfCoverage -I {} -L /home/BIO/johnw/GELCC_WES_RAW_Data/Agilent_SureSelect_v5UTRs_edited.bed \
 -R /home/BIO/johnw/GELCC_WES_RAW_Data/ucsc.hg19.fasta \
 -dt BY_SAMPLE -dcov 5000 -l INFO --omitDepthOutputAtEachBase --omitLocusTable \
@@ -21,16 +22,29 @@ find /home/BIO/johnw/GELCC_WES_RAW_Data/2_GELCC_bam_files -name "*.bam" | java -
 --countType COUNT_FRAGMENTS \
 -o {.}.out'
 
-java -Xmx3072m -jar ./Sting/dist/GenomeAnalysisTK.jar \
--T DepthOfCoverage -I group1.READS.bam.list -L EXOME.interval_list \
--R ./human_g1k_v37.fasta \
--dt BY_SAMPLE -dcov 5000 -l INFO --omitDepthOutputAtEachBase --omitLocusTable \
---minBaseQuality 0 --minMappingQuality 20 --start 1 --stop 5000 --nBins 200 \
---includeRefNSites \
---countType COUNT_FRAGMENTS \
--o group1.DATA
+############################ I need to use the GATK 4.0 documentation################################3333
+https://software.broadinstitute.org/gatk/documentation/tooldocs/4.0.5.0/org_broadinstitute_hellbender_tools_CountReads.php
 
+find /home/BIO/johnw/GELCC_WES_RAW_Data/2_GELCC_bam_files -name "*.bam" | 
 
+java -jar GenomeAnalysisTK.jar \
+   -T DepthOfCoverage \
+   -R reference.fasta \
+   -o 24500a1d7cf4487dbc1053197f3f3e73.out\
+   -I /home/BIO/johnw/GELCC_WES_RAW_Data/2_GELCC_bam_files/24500a1d7cf4487dbc1053197f3f3e73.bam\
+   -L /home/BIO/johnw/GELCC_WES_RAW_Data/Agilent_SureSelect_v5UTRs_edited.bed\
+   -dt BY_SAMPLE \
+   -dcov 5000 \
+   -l INFO \
+   --omitDepthOutputAtEachBase \
+   --omitLocusTable \
+   --minBaseQuality 0 \
+   --minMappingQuality 20 \
+   --start 1 \
+   --stop 5000 \
+   --nBins 200 \
+   --includeRefNSites \
+   --countType COUNT_FRAGMENTS
 
 
 
